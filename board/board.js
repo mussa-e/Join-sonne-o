6892ccaa142ca2ebe69c40b7db9d3052de80fe6a
Mussa Faragi <mussa.faragi@gmx.de>
2025-12-12 22:59:50 +0100

    board responsiv

--- board/board.js
+++ board/board.js
@@ -10,3 +10,19 @@ async function loadTasks() {
   });
 }
 
+
+function responsivAddButton() {
+  const respButton = document.getElementById('add-task-btn');
+    
+
+  if (window.matchMedia("(max-width: 900px)").matches) {
+      
+      respButton.textContent = "";
+        
+  } else {
+      respButton.textContent = "Add Task ";
+      
+    }
+}
+responsivAddButton();
+window.addEventListener('resize', responsivAddButton);
